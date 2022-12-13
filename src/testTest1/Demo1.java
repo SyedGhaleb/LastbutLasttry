3da0aac29c7426385f80661a111a685a1366d132
ghalib <syedghalib785@gmail.com>
2022-12-13 21:33:42 +0530

    second branch

--- src/testTest1/Demo1.java
+++ src/testTest1/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The new branch");
-
+		System.out.println("The code is brach2");
 	}
 
 }
