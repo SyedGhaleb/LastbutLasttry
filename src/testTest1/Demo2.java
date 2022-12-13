a0fda12b0200a2374ac6cf8788cbf84d599fb2f9
ghalib <syedghalib785@gmail.com>
2022-12-13 22:05:52 +0530

    last upadte

---
+++ src/testTest1/Demo2.java
@@ -0,0 +1,13 @@
+package testTest1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The new branch");
+		System.out.println("The code is brach2");
+
+	}
+
+}
